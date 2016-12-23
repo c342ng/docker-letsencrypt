d0d4e250bd59f23d0f75ae6f49cc48f853500bbe
chentongming <chentongming@miyabaobei.com>
2016-12-23 13:59:23 +0800

    base

--- bin/justdofun-com.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-certbot certonly --webroot -w /tmp/ -d justdofun.com -d www.justdofun.com -d intra.justdofun.com -d api.justdofun.com -d api.video.justdofun.com -d intra.video.justdofun.com -t
